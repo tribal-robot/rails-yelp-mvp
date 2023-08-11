35779f53e59e71530bb212902a1fd44bc2a7d6ce
Josh Wills <jcwills369@gmail.com>
2023-08-11 11:09:52 +0100

    added models to yelp-mvp

---
+++ app/models/restaurant.rb
@@ -0,0 +1,11 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+
+  VALID_CATEGORIES = %w[chinese italian japanese french belgian]
+
+  validates :category, inclusion: { in: VALID_CATEGORIES }
+end
